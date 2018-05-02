c2bd7b11529a703671f9b2da712e442a0253da9e
Austin Roberts <austin.Roberts72@yahoo.com>
2018-05-01 20:57:01 -0400

    added in content

---
+++ app.js
@@ -0,0 +1,8 @@
+function doesItWork() {
+    $("#output").html('<h1>It\'s Working!!!</h1>');
+}
+
+function areWeSure() {
+    $('#bodyWrapper').css('background-color', 'red');
+    $('#output2').html('<h1>Is the background now red?</h1>');
+}
